6fdeb86e9fd41da3eb6c835012f40e87ac891949
marioavaj <predaj@predaj-tonerov.sk>
2022-04-29 11:50:51 +0200

    2-klik vypnutie bannerov

---
+++ banner.js
@@ -0,0 +1,14 @@
+function carHidden2() {
+  let vitara = document.getElementById("banner2");
+  vitara.style.display = "none";
+}
+
+function carHidden1() {
+  let swage = document.getElementById("banner1");
+  swage.style.display = "none";
+}
+
+function carHidden3() {
+  let swift = document.getElementById("banner3");
+  swift.style.display = "none";
+}
